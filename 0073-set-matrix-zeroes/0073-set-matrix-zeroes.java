2d2e84103b71977a8dbb6f74245c2bc366318fa4
tanweerkamal425 <99184799+tanweerkamal425@users.noreply.github.com>
2024-07-20 17:00:23 +0530

    Time: 1 ms (81.17%), Space: 45.5 MB (56.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,6 +5,9 @@ class Solution {
         int col = matrix[0].length;
         int col0 = 1;
         
+        //traverse through the array and for non-zero cell mark 1st row as 0 and if the 
+        //cell is not in first column mark first column as 0 and if cell is first column 
+        //mark col0 as 0
         for (int i = 0; i < row; ++i) {
             for (int j = 0; j < col; ++j) {
                 if (matrix[i][j] == 0) {
@@ -19,6 +22,7 @@ class Solution {
             }
         }
         
+        //traverse from 1st row and 1st column and mark cells as zero
         for (int i = 1; i < row; ++i) {
             for (int j = 1; j < col; ++j) {
                 if (matrix[i][j] != 0) {
@@ -29,12 +33,15 @@ class Solution {
             }
         }
         
+        
+        //mark the first row as zero
         if (matrix[0][0] == 0) {
             for (int j = 0; j < col; ++j) {
                 matrix[0][j] = 0;
             }
         }
         
+        //then mark the first col as zero
         if (col0 == 0) {
             for (int i = 0; i < row; ++i) {
                 matrix[i][0] = 0;
