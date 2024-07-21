0968a08ebce489409d7d57d893fa1456b6d9e988
tanweerkamal425 <99184799+tanweerkamal425@users.noreply.github.com>
2024-07-21 10:37:17 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (100.00%) - LeetHub

--- 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.java
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minChanges(int n, int k) {
         int cnt = 0;
-        
+        //for int traverse 32 bits if bit is 1 in n and 0 in k cnt increases else if bit is 0 in n and 1 in k conversion is not possible
         for (int i = 0; i < 32; ++i) {
             int a = (n & (1 << i));
             int b = (k & (1 << i));
