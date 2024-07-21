99b3db014ed5597ef718ed41fa156485d41a4d34
tanweerkamal425 <99184799+tanweerkamal425@users.noreply.github.com>
2024-07-21 13:05:45 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (50.00%) - LeetHub

---
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String losingPlayer(int x, int y) {
+        int i = 0;
+        while (x >= 1 && y >= 4) {
+            x -= 1;
+            y -= 4;
+            i++;
+        }
+        
+        return i % 2 == 1 ? "Alice" : "Bob";
+    }
+}
